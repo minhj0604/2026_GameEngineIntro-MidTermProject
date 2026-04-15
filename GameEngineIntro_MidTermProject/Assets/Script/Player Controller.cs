c29896d8ee61978a6ac957c955616ac1eb7b03aa
minhj0604 <minhyo0311@gmail.com>
2026-04-15 15:58:11 +0900

    [Feat] 스테이지 레벨 디자인(임시) 및 코드 작성

--- GameEngineIntro_MidTermProject/Assets/Script/Player Controller.cs
+++ GameEngineIntro_MidTermProject/Assets/Script/Player Controller.cs
@@ -1,6 +1,7 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -22,6 +23,16 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
@@ -33,10 +44,26 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputValue value)
     {
-        if(value.isPressed && isGrounded)
+        if (value.isPressed && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveToNextLevel();
+        }
+        if (collision.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
---
+++ GameEngineIntro_MidTermProject/Assets/Script/TilemapDisabler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TilemapDisabler : MonoBehaviour
+{
+  
+    private void Awake()
+    {
+        GetComponent<TilemapDisabler>().enabled = false;
+    }
+}
